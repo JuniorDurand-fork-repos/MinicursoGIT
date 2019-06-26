93762b93572736aab61812f1a1f03376d64712e7
Fernando Henrique <fernandodojo@gmail.com>
2019-06-26 08:23:10 -0300

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+import soma as s
+import subtracao as sub
+def calculadora(a, b, operacao):
+	if operacao == "+":
+		print(s.soma(a,b))
+	elif operacao == "-":
+		print(sub.sub(a,b))
+
+
+x = int(input("Digite o primeiro valor"))
+y = int(input("Digite o segundo valor"))
+op = input("Digite a operação(+, -)")
+
+calculadora(x, y, op)
